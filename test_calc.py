dff923481e6f6d7e98eeab64c28b23da8a3e1585
Jackson Wold <jacksonrwold@gmail.com>
2021-11-18 14:09:32 -0600

    added more to the test divide

--- test_calc.py
+++ test_calc.py
@@ -22,6 +22,12 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(calc.divide(10, 5), 2)
         self.assertEqual(calc.divide(-1, 1), -1)
         self.assertEqual(calc.divide(-1, -1), 1)
+        self.assertEqual(calc.divide(5, 2), 2.5)
+
+        # self.assertRaises(ValueError, calc.divide, 10, 0) other way to test for exceptions vvv
+
+        with self.assertRaises(ValueError):
+            calc.divide(10, 0)
 
 if __name__ == '__main__':
     unittest.main()
